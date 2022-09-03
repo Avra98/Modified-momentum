a381df2b891b491aa7d0d0dbec8c4e7a4a2635b7
Avra98 <69806857+Avra98@users.noreply.github.com>
2022-09-03 12:14:07 -0400

    Add files via upload

--- example/train_sgd.py
+++ example/train_sgd.py
@@ -1,5 +1,3 @@
-
-
 import argparse
 import torch
 
@@ -22,20 +20,20 @@ from sgd import SGD
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--adaptive", default=True, type=bool, help="True if you want to use the Adaptive SAM.")
-    parser.add_argument("--batch_size", default=128, type=int, help="Batch size used in the training and validation loop.")
+    parser.add_argument("--batch_size", default=100, type=int, help="Batch size used in the training and validation loop.")
     parser.add_argument("--depth", default=4, type=int, help="Number of layers.")
     parser.add_argument("--dropout", default=0.0, type=float, help="Dropout rate.")
-    parser.add_argument("--epochs", default=200, type=int, help="Total number of epochs.")
+    parser.add_argument("--epochs", default=300, type=int, help="Total number of epochs.")
     parser.add_argument("--label_smoothing", default=0.1, type=float, help="Use 0.0 for no label smoothing.")
-    parser.add_argument("--learning_rate", default=0.1, type=float, help="Base learning rate at the start of the training.")
-    parser.add_argument("--momentum", default=0.8, type=float, help="SGD Momentum.")
+    parser.add_argument("--learning_rate", default=1e-1, type=float, help="Base learning rate at the start of the training.")
+    parser.add_argument("--momentum", default=0.2, type=float, help="SGD Momentum.")
     parser.add_argument("--threads", default=2, type=int, help="Number of CPU threads for dataloaders.")
     parser.add_argument("--rho", default=2.0, type=int, help="Rho parameter for SAM.")
-    parser.add_argument("--weight_decay", default=0.0005, type=float, help="L2 weight decay.")
+    parser.add_argument("--weight_decay", default=0.0000, type=float, help="L2 weight decay.")
     parser.add_argument("--width_factor", default=2, type=int, help="How many times wider compared to normal ResNet.")
     args = parser.parse_args()
 
-    initialize(args, seed=42)
+    initialize(args, seed=2)
     device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     torch.cuda.empty_cache()
 
@@ -43,8 +41,8 @@ if __name__ == "__main__":
     log = Log(log_each=10)
     model = WideResNet(args.depth, args.width_factor, args.dropout, in_channels=3, labels=10).to(device)
 
-    optimizer = SGD(model.parameters(),lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
-    #optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum)
+    #optimizer = SGD(model.parameters(),lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum)
     scheduler = StepLR(optimizer, args.learning_rate, args.epochs)
 
     for epoch in range(args.epochs):
@@ -53,10 +51,11 @@ if __name__ == "__main__":
 
         for batch in dataset.train:
             inputs, targets = (b.to(device) for b in batch)
-
             # first forward-backward step
             enable_running_stats(model)
             predictions = model(inputs)
+
+            optimizer.zero_grad()
             loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
             loss.mean().backward()
             optimizer.step()
@@ -73,7 +72,6 @@ if __name__ == "__main__":
         with torch.no_grad():
             for batch in dataset.test:
                 inputs, targets = (b.to(device) for b in batch)
-
                 predictions = model(inputs)
                 loss = smooth_crossentropy(predictions, targets)
                 correct = torch.argmax(predictions, 1) == targets
